caa9efeff0b7438926235b88ef1cf979775b30d2
Roy <roy_2000debie@hotmail.com>
2020-06-02 15:10:50 +0200

    Dobbelsteen succesvol toegevoegd aan de pion verplaatsen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// vanaf hier komt de code voor de pion gemaakt door Roy
+
 var pion = document.querySelector("#pion");
 var container = document.querySelector("#contentContainer");
 var xcoord = document.getElementById("xcoord");
@@ -40,7 +42,31 @@ function getClickPosition(e) {
   xcoord.innerHTML = "X coordinaten: " + vwcall;
   ycoord.innerHTML = "Y coordinaten: " + vhcall;
   vakje.innerHTML = "Vakje: " + numbvakje;
-
-
-
 }
+
+// vanaf hier komt de code voor de dobbelsteen gemaakt door Canisha
+
+var aantalVakjes = 0;
+var string = "";
+
+function rollDice() {
+  const dice = document.getElementById("die-list");
+  toggleClasses(dice);
+  dice.dataset.roll = getRandomNumber(1,6);
+  aantalVakjes = dice.dataset.roll;
+  string = "U mag " + aantalVakjes + " vakjes vooruit!";
+  document.getElementById("vakjes").innerHTML = string;
+}
+
+function toggleClasses(dice) {
+  dice.classList.toggle("odd-roll");
+  dice.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById("dobbelsteen").addEventListener("click", rollDice);
