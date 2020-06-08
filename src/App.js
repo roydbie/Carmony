39cb889b47acbb853496dc0fd8593c1924e99017
Tommy <421184@student.fontys.nl>
2020-06-08 16:11:47 +0200

    Eerste opzet bootstrap
    React app opgezet met bootstrap en yarn. Pagina's aangemaakt Home, Contact, Spelpagina, uitleg en nomatch

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import { Home } from './Home';
+import { Contact } from './Contact';
+import { Uitleg } from './Uitleg';
+import { NoMatch } from './NoMatch';
+import { SpelPagina} from './SpelPagina';
+import { Layout } from './components/Layout';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Layout>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/spelpagina" component={SpelPagina} />
+          <Route path="/uitleg" component={Uitleg} />
+          <Route component={NoMatch} />
+
+        </Switch>
+      </Router>
+      </Layout>
+
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/SpelPagina.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const SpelPagina = () => (
+
+    <div>
+        <h2>Spelpagina</h2>
+    </div>
+)
