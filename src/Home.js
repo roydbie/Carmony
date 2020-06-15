1789d7868b258f2167e8e21b81d071a20f2d241a
Tommy <421184@student.fontys.nl>
2020-06-15 15:08:32 +0200

    Terug knop toegevoegd + wat css aangepast
    Terug knop aan alle pagina's toegevoegd en ik heb ook de hover effects van de Speel en terug knop wat aangepast en een shadow eraan toegevoegd

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,22 @@ import history from './history';
 
 import "./styles.css";
 import { Button } from "./components/Button";
+import { FaAngleLeft } from "react-icons/fa";
 
 export const Home = () => (
     <div className="App">
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-        <Button onClick={() => { history.push('/spelpagina') }} type="button" buttonStyle="btn--danger--solid" buttonSize="btn--large">
-            Speel
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+
+            <Button onClick={() => { history.push('/spelpagina') }} type="buttonIcon" buttonStyle="btn--danger--solid" buttonSize="btn--large">
+                Speel
         </Button>
+
+        </div>
+
+        <div style={{ position: "absolute", top: 0, left: 0 }}>
+            <Button onClick={() => { history.goBack() }} style="text-align: center;" type="button" buttonStyle="backbtn--icon--outline" buttonSize="btn--medium">
+                <FaAngleLeft />
+            </Button>
         </div>
     </div>
 
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import history from './history';
+
+import "./styles.css";
+import { Button } from "./components/Button";
+import { FaAngleLeft } from "react-icons/fa";
 
 export const Login = () => (
 
-    <div>
-        <h2>Login</h2>
-        
+    <div className="App">
+        <div style={{ position: "absolute", top: 0, left: 0 }}>
+            <Button onClick={() => { history.goBack() }} style="text-align: center;" type="button" buttonStyle="backbtn--icon--outline" buttonSize="btn--medium">
+                <FaAngleLeft />
+            </Button>
+        </div>
+
+        <div><h2>Login</h2></div>
     </div>
 )
