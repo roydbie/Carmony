e225fef4fddd4faae956ca8123bcdeb66614c49a
416399 <47740087+416399@users.noreply.github.com>
2020-06-22 10:31:33 +0200

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import history from './history';
+
+import "./styles.css";
+import { Button } from "./components/Button";
+import { FaAngleLeft } from "react-icons/fa";
+
+export const Contact = () => (
+
+    <div className="App">
+        <div style={{ position: "absolute", top: 0, left: 0 }}>
+            <Button onClick={() => { history.goBack() }} type="button" buttonStyle="backbtn--icon--outline" buttonSize="btn--medium">
+                <FaAngleLeft />
+            </Button>
+        </div>
+
+<div><h2>Contact</h2></div>
+        
+    </div>
+)
